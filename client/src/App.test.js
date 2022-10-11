2b9e33427b2c6e66b2bbda2de4711b3d505d0267
Anna Shylkina <anna.star.200036@gmail.com>
2022-10-11 23:35:00 +0300

    add test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const Client = require("socket.io-client");
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("my awesome project", () => {
+  let io, serverSocket, clientSocket;
+
+  beforeAll((done) => {
+    const httpServer = createServer();
+    io = new Server(httpServer);
+    httpServer.listen(() => {
+      const port = httpServer.address().port;
+      clientSocket = new Client(`http://localhost:${port}`);
+      io.on("connection", (socket) => {
+        serverSocket = socket;
+      });
+      clientSocket.on("connect", done);
+    });
+  });
+
+  afterAll(() => {
+    io.close();
+    clientSocket.close();
+  });
+
+  test("should work", (done) => {
+    clientSocket.on("hello", (arg) => {
+      expect(arg).toBe("world");
+      done();
+    });
+    serverSocket.emit("hello", "world");
+  });
+
+  test("should work (with ack)", (done) => {
+    serverSocket.on("hi", (cb) => {
+      cb("hola");
+    });
+
+    clientSocket.emit("hi", (arg) => {
+      expect(arg).toBe("hola");
+      done();
+    });
+  });
 });
